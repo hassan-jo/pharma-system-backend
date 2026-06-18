1c82d3942d033a295f6c99118fc8c811a8bb782e
hassan-jo <hassanmoahmed09@gmail.com>
2026-06-18 13:06:13 +0300

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors({ origin: '*', credentials: true }));
+app.use(cors({ origin: 'https://pharma-system-frontend.vercel.app', credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
